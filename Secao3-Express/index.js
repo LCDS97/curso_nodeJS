115ee439de8b76ac188bfce186e662412c83db2f
LCDS97 <lucas.cds1997@gmail.com>
2021-03-14 12:18:23 -0300

    UP_Aula

--- Secao3-Express/index.js
+++ Secao3-Express/index.js
@@ -4,6 +4,31 @@ const express = require("express");
 // Iniciando o express e passando para a constante "app"
 const app = express();
 
+// Criando uma rota inicial
+app.get("/", function(req, res){
+    res.send("Bem vindo a rota inicial")
+});
+
+app.get("/blog", function(req,res){
+    res.send("Bem vindo ao meu blog!");
+});
+
+app.get("/canal/youtube", function(req, res) {
+    res.send("Bem vindo ao meu canal!");
+});
+
+// Rota com parametro
+// Só é acessivel com o parametro (obrigatório)
+app.get("/ola/:nome", function(req, res) {
+// REQ => Dados enviados pelo usuário
+// RES => Resposa que vai ser enviada para o usuário
+    res.send(req.params.nome);
+});
+
+
+
+
+
 app.listen(4000, function(erro){
     if(erro){
         console.log("Ocorreu um erro!");
