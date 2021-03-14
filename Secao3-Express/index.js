37ebbfcb4e5c39f58a03ecd3f4218d9dc24e1e90
LCDS97 <lucas.cds1997@gmail.com>
2021-03-14 15:05:22 -0300

    Terminado módulo express

--- Secao3-Express/index.js
+++ Secao3-Express/index.js
@@ -9,22 +9,47 @@ app.get("/", function(req, res){
     res.send("Bem vindo a rota inicial")
 });
 
-app.get("/blog", function(req,res){
-    res.send("Bem vindo ao meu blog!");
+app.get("/blog/:artigo?", function(req,res){
+
+    var artigo = req.params.artigo;
+
+    if(artigo){
+        res.send("<h2>Artigo: " + artigo + "</h2>");
+    }
+    else{
+        res.send("Bem vindo ao meu blog!");
+    }
+
 });
 
 app.get("/canal/youtube", function(req, res) {
-    res.send("Bem vindo ao meu canal!");
+    var canal = req.query["canal"];
+
+    if(canal) {
+        res.send(canal)
+    }
+    else {
+        res.send("Nenhum canal fornecido!")
+    }
+;
 });
 
 // Rota com parametro
 // Só é acessivel com o parametro (obrigatório)
-app.get("/ola/:nome", function(req, res) {
+app.get("/ola/:nome/:empresa", function(req, res) {
 // REQ => Dados enviados pelo usuário
 // RES => Resposa que vai ser enviada para o usuário
-    res.send(req.params.nome);
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send("<h1>Oi " + nome + " da " + empresa + " </h1>");
 });
 
+// Parametros obrigatório em rota
+// Somente necessita de colocar na rota Ex: app.get("/(rota)/:(parametro)")
+
+// Para adicionar parametros opcionais
+// Basta colocar no parametro no final o (?)
+// Ex: app.get("/(rota)/:(parametro)?")
 
 
 
