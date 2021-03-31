93bc3a3ec745698624586eee2b32967bd45ef853
LCDS97 <lucas.cds1997@gmail.com>
2021-03-30 23:02:16 -0300

    Criado usuarios e aplicado jwt ao projeto

--- Secao13-API/index.js
+++ Secao13-API/index.js
@@ -3,7 +3,13 @@ const app = express();
 const bodyParser = require("body-parser");
 // CORS para permimtir requisições externas
 const cors = require("cors");
+// Json Web Token para gerar token nas autenticações
+const jwt = require("jsonwebtoken");
+// Criando segredo para gerar web token, NÃO PODE SER DIVULGADA DE NENHUMA FORMA
+const JWTSecret = "algosecretoguardadoA7ChavesNinguemVaiDescobrirhahaharsrs"
 
+
+// Habilitando api sendo acessada externamente
 app.use(cors());
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -31,6 +37,21 @@ var DB = {
             year: 2012,
             price: 20
 
+        },
+    ],
+    users: [
+        {
+            // Cadastrando alguns usuários para utilizar no JWT
+            id: 1,
+            name: "Lucas",
+            email: "lucas@guiaprogramador.com",
+            password: "1234"
+        },
+        {
+            id: 2,
+            name: "Amanda",
+            email: "amanda@guiaprogramador.com",
+            password: "1234"
         },
     ]
 }
@@ -127,6 +148,49 @@ app.put("/game/:id",(req, res) => {
         else {
             res.sendStatus(404);
         }
+    }
+
+});
+
+// Criando rota de autenticação (Endpoint da API)
+
+app.post("/auth", (req,res) => {
+    
+    var { email,password} = req.body;
+
+    if(email != undefined) {
+
+        var user = DB.users.find(u => u.email == email);
+
+        if(user != undefined){
+            if(user.password == password){
+
+                // Armazenando informações do usuário para saber quem é o dono do token gerado
+                // É chamado de payload, são informações que são enviado do usuário junto com o token
+                jwt.sign({ id: user.id, email: user.email },JWTSecret,{expiresIn:'48h'},(err, token) => { // Chamando o segredo do token e colocando tempo de expiração do token
+                    if(err){
+                        res.status(400);
+                        res.json({err: "Falha interna"});
+                    }else {
+                        res.status(200);
+                        res.json({token: token})
+                    }
+
+                })
+
+
+            }else{
+                res.status(401)
+                res.json({err: "Credenciais inválidas!"});
+            }
+        }else {
+            res.status(404)
+            res.json({err: "E-mail não existente na base de dados!"})
+        }
+
+    }else {
+        res.status(400)
+        res.json({err: "E-mail Invalido"})
     }
 
 });
