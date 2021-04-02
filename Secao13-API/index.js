10b8f5817f57fa64cd808d1d9a6b260fc2edec8f
LCDS97 <lucas.cds1997@gmail.com>
2021-04-02 14:11:55 -0300

    Terminei módulo de JWT
    Aprendido conceitos para configurar token de autorização para utilizar recursos da API aprendido na aula 13, utilizando a biblioteca do Axios

--- Secao13-API/index.js
+++ Secao13-API/index.js
@@ -88,7 +88,7 @@ var DB = {
 
 app.get("/games", auth, (req, res) => {
     res.statusCode = 200;
-    res.json({user: req.loggedUser, games: DB.games});
+    res.json(DB.games);
     
 });
 
--- Secao14-Axios/app.js
+++ Secao14-Axios/app.js
@@ -1,3 +1,39 @@
+// Criando a variavel de config do axios com o Token Bearer e token gerado no login armazenado no local storage
+var axiosConfig = {
+    headers: {
+        Authorization: "Bearer " + localStorage.getItem("token")
+    }
+}
+
+// Função de login
+function login(){
+    // Pegando valores do login
+    var emailField = document.getElementById("email");
+    var passwordField = document.getElementById("password");
+
+    var email = emailField.value;
+    var password = passwordField.value;
+
+    // Fazendo requisição post com a rota de autenticação
+    axios.post("http://localhost:4000/auth",{
+        email,
+        password
+    }).then(res => {
+        var token = res.data.token;
+        // Utilizando o localstorage que é o armazenamento interno do browser para armazenar o token
+        // O único momento que é cancelado é quando limpa o histórico, cookie ou desinstala o navegador
+        localStorage.setItem("token",token);
+        // Recalculando a variavel de token
+        axiosConfig.headers.Authorization = "Bearer " + localStorage.getItem("token");
+    }).catch(err => {
+        alert("DEU PRA LOGAR NÃO!")
+    })
+
+}
+
+
+
+
 // Função de criar um jogo
 function createGame(){
     // Coletando valores do input e atribuindo a uma variavel
@@ -12,7 +48,7 @@ function createGame(){
         price: priceInput.value,
     }
     // Fazendo requisição utilizando o post para criar game da API
-    axios.post("http://localhost:4000/game",game).then(response => {
+    axios.post("http://localhost:4000/game",game,axiosConfig).then(response => {
 
         if(response.status == 200){
             alert("Game cadastrado!")
@@ -27,7 +63,7 @@ function createGame(){
 function deleteGame(listItem){
     var id = listItem.getAttribute("data-id");
     // Utilizando a rota de delete da API com Axios
-    axios.delete("http://localhost:4000/game/"+id).then(response => {
+    axios.delete("http://localhost:4000/game/"+id,axiosConfig).then(response => {
         alert("Game deletado!");
     }).catch(err => {
         console.log(err);
@@ -66,7 +102,7 @@ function updateGame(){
 
         var id = idInput.value;
         // Fazendo requisição utilizando o put para editar o game da API
-        axios.put("http://localhost:4000/game/"+id,game).then(response => {
+        axios.put("http://localhost:4000/game/"+id,game,axiosConfig).then(response => {
     
             if(response.status == 200){
                 alert("Game atualizado!")
@@ -78,7 +114,7 @@ function updateGame(){
 }
 
 // Retornando dados da API da seção 13 com Axios
-axios.get("http://localhost:4000/games").then(response => {
+axios.get("http://localhost:4000/games", axiosConfig).then(response => {
     // Armazenando dados do response em array numa variavel
     var games = response.data;
 
