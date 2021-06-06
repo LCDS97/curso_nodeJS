9b66b76159cac0bbcb392f27ef78f4b9bccaa5b3
LCDS97 <lucas.cds1997@gmail.com>
2021-06-06 19:22:51 -0300

    Adicionando classe de Tabela do projeto

--- Projetos/ExcelForHTMLorPDF/app.js
+++ Projetos/ExcelForHTMLorPDF/app.js
@@ -1,11 +1,18 @@
 var Reader = require("./Reader");
 var Processor = require("./Processor");
+var Table = require("./Table");
 
 var leitor = new Reader();
 
 async function main() {
     var dados = await leitor.Read("./users.csv");
-    Processor.Process(dados)
+    var dadosProcessados = Processor.Process(dados);
+
+    var usuarios = new Table(dadosProcessados);
+
+    console.log(usuarios);
+
+
 }
 
 main();
