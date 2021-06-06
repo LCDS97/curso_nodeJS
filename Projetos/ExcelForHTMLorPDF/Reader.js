9507399f1506d9fa2715c2737e42e1420bb90af0
LCDS97 <lucas.cds1997@gmail.com>
2021-06-06 18:13:55 -0300

    Leitor do projeto

--- Projetos/ExcelForHTMLorPDF/Reader.js
+++ Projetos/ExcelForHTMLorPDF/Reader.js
@@ -1,16 +1,35 @@
+// ANCHOR promisify
+// SECTION Promisify - Conceito
 const fs = require('fs');
+const util = require("util");
+
+// NOTE O promisify transforma uma callback é uma promessa
+// NOTE Ex: var novaFuncaoComPromise = util.promisify(funcaoVelhaComCallback)
+
 
 class Reader {
 
-    Read(filepath){
-        fs.readFile(filepath,"utf8",(err, data) => {
+    constructor(){
+        this.reader = util.promisify(fs.readFile);
+    }
+
+async Read(filepath){
+// NOTE Antigo jeito de do readFile mas não retornando dados
+        /* fs.readFile(filepath,"utf8",(err, data) => {
             if(err){
                 console.log(err);
             }
             else {
-                console.log(data);
+                return data;
             }
-        })
+        }) */
+
+// NOTE Jeito correto de utilizar o Promisify
+        try {
+            return await this.reader(filepath,"utf8");
+        } catch (error) {
+            return undefined;
+        }
     }
 }
 
